b33066998df4a5c7c8f3bd1e41a7292f77c551b9
RochaGabriell <rochagabriell.dev@gmail.com>
2023-02-01 08:47:06 -0300

    medic_profile

--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Aqui estamos dizendo ao Django que dentro do admin poderemosacessar o diretório de media e o static
+    path('', include('medicSearch.urls.HomeUrls')),
+    path('profile/', include('medicSearch.urls.ProfileUrls')),
+    path('medic/', include('medicSearch.urls.MedicUrls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Aqui estamos dizendo ao Django que dentro do admin poderemos acessar o diretório de media e o static
