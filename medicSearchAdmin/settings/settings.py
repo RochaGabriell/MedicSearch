b158f9002902c09c05961d9b51110ccb9636d2e4
RochaGabriell <rochagabriell.dev@gmail.com>
2023-02-18 09:28:10 -0300

    Gunicorn

--- medicSearchAdmin/settings/settings.py
+++ medicSearchAdmin/settings/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!k7pqk6vz(mvyjuxhlv&7v#ys(e6@gf*b7np0^y&sfy%855-i!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 # Application definition
 
