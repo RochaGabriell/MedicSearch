16257c677a2442b71b831c5b7dbaedd677a243ac
RochaGabriell <rochagabriell.dev@gmail.com>
2023-02-22 23:39:37 -0300

    Home
    home_view add context

--- medicSearch/views/HomeView.py
+++ medicSearch/views/HomeView.py
@@ -1,6 +1,22 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
-
+from medicSearch.models.Speciality import Speciality
+from medicSearch.models.State import State
+from medicSearch.models.City import City
+from medicSearch.models.Neighborhood import Neighborhood
 
 def home_view(request):
-    return render(request, template_name='home/home.html', status=200)
+    
+    specialties = Speciality.objects.all()
+    states = State.objects.all()
+    cities = City.objects.all()
+    neighborhoods = Neighborhood.objects.all()
+    
+    context = {
+        'specialties': specialties,
+        'states': states,
+        'cities': cities,
+        'neighborhoods': neighborhoods
+    }
+
+    return render(request, template_name='home/home.html', context=context, status=200)
