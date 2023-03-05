e0555debba011c1b3aa4322f598f0d25808ed3d6
RochaGabriell <rochagabriell.dev@gmail.com>
2023-03-05 16:57:42 -0300

    SETTINGS_MODULE

--- medicSearchAdmin/settings/production.py
+++ medicSearchAdmin/settings/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_PRODUCTION')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'medicsearch.up.railway.app']
+ALLOWED_HOSTS = ['medicsearch.up.railway.app']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
