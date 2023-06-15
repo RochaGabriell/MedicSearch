4fa3212db0741837681d26374e9e97906ff9ef0d
RochaGabriell <rochagabriell.dev@gmail.com>
2023-06-15 11:03:44 -0300

    :bug: Fix a bug.

--- medicSearchAdmin/settings/settings.py
+++ medicSearchAdmin/settings/settings.py
@@ -109,7 +109,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "../medicSearch/base_static",
+]
+
+STATIC_ROOT = BASE_DIR / "static" # collectstatic
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
